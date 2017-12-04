5b4854342313dae53fd131f0ab66d6716c4f2eb0
Michael Walker <mike@barrucadu.co.uk>
2017-12-04 23:54:56 +0000

    Give packages and github repos in a config file

--- Main.hs
+++ Main.hs
@@ -1,4 +1,45 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
+import           Data.Either        (lefts, rights)
+import           System.Environment (getArgs)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = getArgs >>= \case
+  [fname] -> toConfig <$> readFileOrStdin fname >>= \case
+    Right packages -> print packages
+    Left errs -> mapM_ putStrLn errs
+  _ -> putStrLn "USAGE: packdeps-github (- | /path/to/file)"
+
+-- | Read stdin if the argument is \"-\", otherwise read the given
+-- file.
+readFileOrStdin :: String -> IO String
+readFileOrStdin "-" = getContents
+readFileOrStdin fname = readFile fname
+
+-- | A package.
+data Package = Package
+  { hackage :: String
+  -- ^ The name on hackage.
+  , github :: String
+  -- ^ The GitHub user and repository name.
+  } deriving Show
+
+-- | Turn a configuration string into a list of packages to check.
+toConfig :: String -> Either [String] [Package]
+toConfig = validate . map go . filter (not . isComment) . lines where
+  go line = case words line of
+    [name, userrepo] -> Right Package { hackage = name, github = userrepo }
+    _ -> Left ("Confusing configuration entry: '" ++ line ++ "'")
+
+  -- strip empty lines or lines starting with a #.
+  isComment ('#':_) = True
+  isComment "" = True
+  isComment _  = False
+
+-- | Accumulate a list of possible errors.
+validate :: [Either a b] -> Either [a] [b]
+validate es = case (lefts es, rights es) of
+  ([], bs) -> Right bs
+  (as, _)  -> Left  as
--- README.markdown
+++ README.markdown
@@ -4,3 +4,12 @@ packdeps-github
 A program to open issues on GitHub repositories when the latest
 release on Hackage does not work with the newest version of all its
 dependencies.
+
+
+Usage
+-----
+
+```
+$ packdeps-github example.config
+[Package {hackage = "packdeps-github", github = "barrucadu/packdeps-github"}]
+```
